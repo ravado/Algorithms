7db36601b5cafef5c4510fe516a31937f61abe5f
Ivan Cherednychok <ivan.cherednychok@nbcuni.com>
2024-02-02 12:01:23 +0200

    Basic structure

---
+++ IC.Algorithms.Sorting/IC.Algorithms.Sorting/MergeSorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IC.Algorithms.Sorting
+{
+    internal class MergeSorter
+    {
+        public int[] Sort(int[] unsortedArray)
+        {
+            int[] sortedArray = unsortedArray;
+            
+            return sortedArray;
+        }
+    }
+}
---
+++ IC.Algorithms.Sorting/IC.Algorithms.Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using IC.Algorithms.Sorting;
+
+int[] unsortedArray = [1, 9, 2, 93, 4, 5, 2, 5];
+int[] sortedArray = [0];
+
+var mergeSorter = new MergeSorter();
+Console.WriteLine("Using MergeSorter to sort: \t" + string.Join(", ", unsortedArray));
+
+sortedArray = mergeSorter.Sort(unsortedArray);
+Console.WriteLine("Sorted: \t\t\t" + string.Join(", ", sortedArray));
+
+Console.ReadKey();
