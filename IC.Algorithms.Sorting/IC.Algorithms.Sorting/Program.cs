27a22b1c3fd32ffe0a8df4b2aca4aa7b5c9f1375
Ivan Cherednychok <the.ravado@gmail.com>
2024-02-04 11:29:58 +0200

    small improvements with numbers generator

--- IC.Algorithms.Sorting/IC.Algorithms.Sorting/Program.cs
+++ IC.Algorithms.Sorting/IC.Algorithms.Sorting/Program.cs
@@ -1,12 +1,12 @@
 ﻿using IC.Algorithms.Sorting;
 
-int[] unsortedArray = [1, 9, 2, 93, 4, 5, 2, 5, 100];
+int[] unsortedArray = RandomArrayGenerator.GetArray(10); //[1, 9, 2, 93, 4, 5, 2, 5, 100];
 int[] sortedArray = [0];
 
 var mergeSorter = new MergeSorter();
 Console.WriteLine("Using MergeSorter to sort: \t" + unsortedArray.Format());
 
 sortedArray = mergeSorter.Sort(unsortedArray);
-Console.WriteLine($"Sorted: \n{unsortedArray.Format()} --> {sortedArray.Format()}");
+Console.WriteLine($"Sorted: \n{unsortedArray.Format()} \n--> \n{sortedArray.Format()}");
 
 Console.ReadKey();
---
+++ IC.Algorithms.Sorting/IC.Algorithms.Sorting/RandomArrayGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IC.Algorithms.Sorting
+{
+    public static class RandomArrayGenerator
+    {
+        static Random _rand = new Random();
+        public static int[] GetArray(int length, int from = 0, int to = 100)
+        {
+            int[] array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = _rand.Next(from, to);
+            }
+
+            return array;
+        }
+    }
+}
